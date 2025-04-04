928a4eb67bf92d80de8ba7d6e063b49f6e83f9a8
Daniel-derf <danielpardinho321@gmail.com>
2025-04-03 21:11:40 -0300

    feat: modal background overlay

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
 
   return (
     <div className="app-container">
+      {isModalOpen && <div className="overlay" onClick={closeModal}></div>}
       <Modal isOpen={isModalOpen} closeModal={closeModal} />
       <h1>CONTENT</h1>
       <button onClick={openModal} className="container-btn">
